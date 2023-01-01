2f629666eda8e0b19d61633a34c10f545d6e375d
caotrungduc <trungdc2003@gmail.com>
2023-01-01 12:50:26 +0700

    fix some bugs

---
+++ src/data/sections.js
@@ -0,0 +1,41 @@
+const SECTIONS_DATA = [
+    {
+        title: 'Hats',
+        imageUrl: '/images/homepage-img/hats.png',
+        id: 1,
+        linkUrl: 'shop/hats'
+
+    },
+    {
+        title: 'Jackets',
+        imageUrl: '/images/homepage-img/jackets.png',
+        id: 2,
+        linkUrl: 'shop/jackets'
+    },
+    {
+        title: 'Sneakers',
+        imageUrl: '/images/homepage-img/sneakers.png',
+        id: 3,
+        linkUrl: 'shop/sneakers'
+    },
+    {
+        title: 'Jewelry',
+        imageUrl: '/images/homepage-img/jewelry.png',
+        id: 4,
+        linkUrl: 'shop/jewelry'
+    },
+    {
+        title: 'Women',
+        imageUrl: '/images/homepage-img/women.png',
+        id: 5,
+        linkUrl: 'shop/women'
+    },
+    {
+        title: 'Men',
+        imageUrl: '/images/homepage-img/men.png',
+        id: 6,
+        linkUrl: 'shop/men'
+    }
+];
+
+export default SECTIONS_DATA;
